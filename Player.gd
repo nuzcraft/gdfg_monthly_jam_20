8610aa5a4e2625a4aa80a552838f3204beafd372
nuzcraft <nuzcraft@gmail.com>
2022-09-05 20:47:32 -0500

    working on player controller

--- Player.gd
+++ Player.gd
@@ -1,19 +1,20 @@
 extends KinematicBody2D
 
-
 export(int) var JUMP_VELOCITY = 120
 export(int) var JUMP_RELEASE_VELOCITY = 60
 export(int) var ACCELERATION = 150
 export(float) var ACCELERATION_DAMPING = 0.15
+export(int) var FRICTION = 400
 export(int) var GRAVITY = 400
 export(int) var EXTRA_GRAVITY = 150
+export(int) var MAX_GRAVITY = 600
+
+enum {
+	MOVE
+}
 
 var velocity = Vector2.ZERO
-
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
+var state = MOVE
 
 
 func _physics_process(delta):
@@ -21,12 +22,17 @@ func _physics_process(delta):
 	input.x = Input.get_axis("ui_left", "ui_right")
 	input.y = Input.get_axis("ui_up", "ui_down")
 	
-	if input.x != 0:
-		velocity.x += ACCELERATION * delta * input.x
-		velocity.x *= pow(1-ACCELERATION_DAMPING, delta*10)
-		
+	match state:
+		MOVE: move_state(input, delta)
+	
+func move_state(input, delta):
+	
+	apply_gravity(delta)
+	
 	if input.x == 0:
-		velocity.x = 0
+		apply_friction(delta)
+	if input.x != 0:
+		apply_acceleration(input, delta)
 		
 	if Input.is_action_just_pressed("ui_up") and is_on_floor():
 		velocity.y = -JUMP_VELOCITY
@@ -34,12 +40,20 @@ func _physics_process(delta):
 	if not is_on_floor():
 		if Input.is_action_just_released("ui_up") and velocity.y < -JUMP_RELEASE_VELOCITY:
 			velocity.y = -JUMP_RELEASE_VELOCITY
-		velocity.y += GRAVITY * delta
+
 		if velocity.y > 0:
 			velocity.y += EXTRA_GRAVITY * delta
-	
-	if is_on_floor():
-		velocity.y == 0
 		
 	velocity = move_and_slide(velocity, Vector2.UP)
+	
+func apply_gravity(delta):
+	velocity.y += GRAVITY * delta
+	velocity.y = min(velocity.y, MAX_GRAVITY)
+	
+func apply_acceleration(input, delta):
+	velocity.x += ACCELERATION * delta * input.x
+	velocity.x *= pow(1-ACCELERATION_DAMPING, delta*10)
+	
+func apply_friction(delta):
+	velocity.x = move_toward(velocity.x, 0, FRICTION * delta)
 		
