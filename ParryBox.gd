e8d7892b1be976faafabf681540d00dfa49c563e
nuzcraft <nuzcraft@gmail.com>
2022-09-20 14:53:01 -0500

    added parrybox, not implemented yet

---
+++ ParryBox.gd
@@ -0,0 +1,16 @@
+extends Area2D
+
+onready var collisionShape := $CollisionShape2D
+
+var is_parrying = false
+
+func start_parry(parry_duration):
+	is_parrying = true
+	collisionShape.set_deferred('disabled', false)
+	yield(get_tree().create_timer(parry_duration), 'timeout')
+	collisionShape.set_deferred('disabled', true)
+	is_parrying = false
+
+
+func _on_ParryBox_area_entered(area):
+	print('parried!')
